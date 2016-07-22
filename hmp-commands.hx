f06269f091767c1bdacc2a68135e8e8be777f155
Markus Armbruster <armbru@redhat.com>
2016-07-22 16:02:37 +0200

    Fix crash bug in rebase of__com.redhat_drive_add
    RH-Author: Markus Armbruster <armbru@redhat.com>
    Message-id: <1469203357-18900-2-git-send-email-armbru@redhat.com>
    Patchwork-id: 71333
    O-Subject: [RHEV-7.2 qemu-kvm-rhev PATCH] Fix crash bug in rebase of__com.redhat_drive_add
    Bugzilla: 1352865
    RH-Acked-by: Laszlo Ersek <lersek@redhat.com>
    RH-Acked-by: Miroslav Rezanina <mrezanin@redhat.com>
    RH-Acked-by: Laurent Vivier <lvivier@redhat.com>
    RH-Acked-by: Fam Zheng <famz@redhat.com>
    
    The traditional monitor command function type is
    
        void (*)(Monitor *mon, const QDict *qdict);
    
    A new type was added for QMP:
    
        void (*)(QDict *params, QObject **ret_data, Error **errp);
    
    The struct describing commands has both types in a union.
    
    The plan back then was to convert everything to this type.  Seemed
    sensible at the time, because it let us use the same function for both
    the QMP and the HMP command.  That turned out to be a bad idea, and we
    moved to separate command definitions.  The (relatively few) HMP
    commands using the second type were eventually converted back to the
    first one.  Support for HMP commands implemented as the second type
    was dropped in upstream commit 8a4f501.  Since then, the monitor core
    calls all HMP commands through union member with the first type.
    
    Meanwhile, we created __com.redhat_drive_add for RHEL-6 (commit
    545d0d8).  Its command handler simple_drive_add() conforms to the
    second type.
    
    This got forward ported to RHEL-7, then rebased a couple of times.
    The last rebase (commit 0806196) is wrong: since we failed to notice
    the semantic conflict with upstream commit 8a4f501, we happily
    continue to initialize the second union member to simple_drive_add,
    even though the monitor core now calls it through the first member.
    Oops.
    
    We fixed the same bug for __com.redhat_drive_del in commit 85786e0 for
    BZ 1341531.  Fix this one the same way: create a separate HMP command
    handler hmp_simple_drive_add(), calling qmp_simple_drive_add(),
    renamed from simple_drive_add().
    
    No more uses of cmd_new in hmp-commands.hx remain.
    
    Signed-off-by: Markus Armbruster <armbru@redhat.com>
    Signed-off-by: Miroslav Rezanina <mrezanin@redhat.com>
    Signed-off-by: Jim Somerville <Jim.Somerville@windriver.com>

--- hmp-commands.hx
+++ hmp-commands.hx
@@ -1340,7 +1340,7 @@ ETEXI
         .args_type  = "simple-drive:O",
         .params     = "id=name,[file=file][,format=f][,media=d]...",
         .help       = "Create a drive similar to -device if=none.",
-        .mhandler.cmd_new = simple_drive_add,
+        .cmd        = hmp_simple_drive_add,
     },
 
 STEXI
