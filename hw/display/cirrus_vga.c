16e23b406e861bc3ec4a306d26c3de79a9af1cfd
Nam Ninh <nam.ninh@windriver.com>
2016-09-07 15:45:19 -0400

    STX: mig-compat: fix vga ram size mismatch
    VGA vram size is mismatched between versions that causes the
    following error and the migration abort:
    
    qemu-system-x86_64: Length mismatch: vga.vram: 0x800000 in != 0x1000000: Invalid argument
    
    Align the sizes to fix the problem.
    
    Signed-off-by: Nam Ninh <nam.ninh@windriver.com>
    Signed-off-by: Jim Somerville <Jim.Somerville@windriver.com>

--- hw/display/cirrus_vga.c
+++ hw/display/cirrus_vga.c
@@ -3061,7 +3061,7 @@ static void isa_cirrus_vga_realizefn(DeviceState *dev, Error **errp)
 
 static Property isa_cirrus_vga_properties[] = {
     DEFINE_PROP_UINT32("vgamem_mb", struct ISACirrusVGAState,
-                       cirrus_vga.vga.vram_size_mb, 16),
+                       cirrus_vga.vga.vram_size_mb, 8),
     DEFINE_PROP_BOOL("blitter", struct ISACirrusVGAState,
                      cirrus_vga.enable_blitter, true),
     DEFINE_PROP_END_OF_LIST(),
@@ -3134,7 +3134,7 @@ static void pci_cirrus_vga_realize(PCIDevice *dev, Error **errp)
 
 static Property pci_vga_cirrus_properties[] = {
     DEFINE_PROP_UINT32("vgamem_mb", struct PCICirrusVGAState,
-                       cirrus_vga.vga.vram_size_mb, 16),
+                       cirrus_vga.vga.vram_size_mb, 8),
     DEFINE_PROP_BOOL("blitter", struct PCICirrusVGAState,
                      cirrus_vga.enable_blitter, true),
     DEFINE_PROP_BOOL("global-vmstate", struct PCICirrusVGAState,
