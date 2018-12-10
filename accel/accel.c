vivier/qemu

Use kvm by default
Bugzilla: 906185

RHEL uses kvm accelerator by default, if available.

Signed-off-by: Miroslav Rezanina <mrezanin@redhat.com>

Rebase note:
 - 2.2: Move code from vl.c to accel.c

accel.c
@@ -87,8 +87,8 @@ int configure_accelerator(MachineState *ms)
 
     p = qemu_opt_get(qemu_get_machine_opts(), "accel");
     if (p == NULL) {
-        /* Use the default "accelerator", tcg */
-        p = "tcg";
+        /* Use kvm accelerator by default, fail to tcg*/
+        p = "kvm:tcg";
     }
 
     while (!accel_initialised && *p != '\0') {

starlingx-staging/stx-qemu

Signed-off-by: Jim Somerville <Jim.Somerville@windriver.com>

accel/accel.c
@@ -79,8 +79,8 @@ void configure_accelerator(MachineState *ms)
 
     accel = qemu_opt_get(qemu_get_machine_opts(), "accel");
     if (accel == NULL) {
-        /* Use the default "accelerator", tcg */
-        accel = "tcg";
+        /* Use kvm accelerator by default, fail to tcg*/
+        accel = "kvm:tcg";
     }
 
     accel_list = g_strsplit(accel, ":", 0);